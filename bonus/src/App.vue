94af4523d082b62a775476bfaafa48352f5250b0
spellcry <sangiothebest@hotmail.com>
2022-09-02 16:05:34 +0200

    creazione cartella bonus

---
+++ bonus/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Title msg="La mia prima app con Vue CLI!"></Title>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Title from './components/Title.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Title
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ bonus/src/components/Hero.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="hero">
+        
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'Hero'
+    }
+
+</script>
